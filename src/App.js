3cc60eea112534e70bdf1aea6c4a09dd5ecf13e1
Gayatri Bhosale <gayatribohosale@gmail.com>
2023-05-21 12:29:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import  FormikComponent from './FormikComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Formik</h1>
+      <FormikComponent/>
+    </div>
+  );
+}
+
+export default App;
